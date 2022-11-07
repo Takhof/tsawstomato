f422cf8e9dbdc16fb9132a027f2c5fb055c9889c
Thomas Hofmann <thomas.t.hofmann@gmail.com>
2022-11-07 20:32:21 +0900

    feat: add material ui theme

---
+++ src/theme-options.ts
@@ -0,0 +1,20 @@
+import { ThemeOptions } from "@material-ui/core/styles/createMuiTheme";
+
+export const themeOptions: ThemeOptions = {
+  palette: {
+    type: "dark",
+    primary: {
+      main: "#5893df",
+    },
+    secondary: {
+      main: "#2ec5d3",
+    },
+    background: {
+      default: "#192231",
+      paper: "#24344d",
+    },
+    text: {
+      primary: "#f3f3f3",
+    },
+  },
+};
--- src/theme.ts
+++ src/theme.ts
@@ -1,6 +1,7 @@
 import { Roboto } from "@next/font/google";
 import { createTheme } from "@mui/material/styles";
 import { red } from "@mui/material/colors";
+import { themeOptions } from "./theme-options";
 
 export const roboto = Roboto({
   weight: ["300", "400", "500", "700"],
@@ -9,21 +10,6 @@ export const roboto = Roboto({
   fallback: ["Helvetica", "Arial", "sans-serif"],
 });
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#556cd6",
-    },
-    secondary: {
-      main: "#19857b",
-    },
-    error: {
-      main: red.A400,
-    },
-  },
-  typography: {
-    fontFamily: roboto.style.fontFamily,
-  },
-});
+const theme = createTheme(themeOptions);
 
 export default theme;
