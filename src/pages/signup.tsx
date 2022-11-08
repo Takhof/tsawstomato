f39198f093bda06756faaa45e6b3ece42c962136
Thomas Hofmann <thomas.t.hofmann@gmail.com>
2022-11-08 20:35:43 +0900

    feat: create signup page

---
+++ src/pages/signup.tsx
@@ -0,0 +1,139 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+import { Button, TextField, Grid } from "@material-ui/core";
+import { Auth } from "aws-amplify";
+import MuiAlert, { AlertProps } from "@mui/material/Alert";
+import Snackbar from "@mui/material/Snackbar";
+import React, { useState } from "react";
+
+interface IFormInput {
+  username: string;
+  password: string;
+  email: string;
+  iceCreamType: { label: string; value: string };
+}
+
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
+  props,
+  ref
+) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+export default function Signup() {
+  const [open, setOpen] = useState(false);
+  const [signUpError, setSignUpError] = useState<string>("");
+
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<IFormInput>();
+
+  const onSubmit: SubmitHandler<IFormInput> = async (data) => {
+    console.log(data);
+    try {
+      signUpCustom(data);
+    } catch (err) {
+      console.error(err);
+      setSignUpError(err.message);
+      setOpen(true);
+    }
+  };
+
+  console.log("Errors:", errors);
+
+  const handleClose = (
+    event?: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  async function signUpCustom(data: IFormInput) {
+    const { username, password, email } = data;
+    try {
+      const { user } = await Auth.signUp({
+        username,
+        password,
+        attributes: {
+          email,
+        },
+        autoSignIn: {
+          enabled: true,
+        },
+      });
+      console.log(user);
+    } catch (error) {
+      console.log("error signing up:", error);
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <p></p>
+      <Grid
+        container
+        direction="column"
+        alignItems="center"
+        justify="center"
+        spacing={1}
+      >
+        <Grid item>
+          <TextField
+            id="username"
+            label="username"
+            variant="outlined"
+            type="text"
+            error={errors.username ? true : false}
+            helperText={errors.username ? errors.username.message : null}
+            {...register("username", {
+              required: { value: true, message: "Please enter a username." },
+              minLength: { value: 3, message: "Minimum length is 3 letters." },
+            })}
+          />
+        </Grid>
+        <Grid item>
+          <TextField
+            id="email"
+            label="email"
+            variant="outlined"
+            type="email"
+            error={errors.email ? true : false}
+            helperText={errors.email ? errors.email.message : null}
+            {...register("email", {
+              required: { value: true, message: "Please enter a valid email." },
+            })}
+          />
+        </Grid>
+        <Grid item>
+          <TextField
+            id="password"
+            label="password"
+            variant="outlined"
+            type="password"
+            error={errors.password ? true : false}
+            helperText={errors.password ? errors.password.message : null}
+            {...register("password", {
+              required: { value: true, message: "Please enter a password." },
+              minLength: { value: 3, message: "Minimum length is 3 letters." },
+            })}
+          />
+        </Grid>
+        <Grid style={{ marginTop: 16 }}>
+          <Button variant="contained" type="submit">
+            Sign up
+          </Button>
+        </Grid>
+      </Grid>
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="error" sx={{ width: "100%" }}>
+          Sign up error!
+        </Alert>
+      </Snackbar>{" "}
+    </form>
+  );
+}
--- src/theme-options.ts
+++ src/theme-options.ts
@@ -1,32 +1,17 @@
 import { ThemeOptions } from "@material-ui/core/styles/createMuiTheme";
-import { Roboto } from "@next/font/google";
-
-export const roboto = Roboto({
-  weight: ["300", "400", "500", "700"],
-  subsets: ["latin"],
-  display: "swap",
-  fallback: ["Helvetica", "Arial", "sans-serif"],
-});
 
 export const themeOptions: ThemeOptions = {
   palette: {
-    type: "dark",
+    type: "light",
     primary: {
-      main: "#5893df",
+      main: "#1976d2",
     },
     secondary: {
-      main: "#2ec5d3",
+      main: "rgb(220, 0, 78)",
     },
     background: {
-      default: "#192231",
-      paper: "#24344d",
-    },
-    text: {
-      primary: "#f3f3f3",
-      secondary: "rgba(255, 255, 255, 0.7)",
-    },
-    typography: {
-      fontFamily: roboto.style.fontFamily,
+      default: "#fff",
+      paper: "#fff",
     },
   },
 };
